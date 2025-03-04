be3edb2ee453965c49c3ed2850e9e93c178fd0a9
olees <olees.orlenko@yandex.ru>
2025-03-03 17:50:38 -0800

    Initial Commit

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Олеся Орленко on 03.03.2025.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
